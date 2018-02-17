db914995b3049f5ddaf98f5873015b6e9bdf9f3e
VojtaDiatka <vojta.diatka@gmail.com>
2018-02-17 20:24:54 +0100

    minor bug fixes

--- js/app.js
+++ js/app.js
@@ -127,8 +127,8 @@ function display_symbol(card) {
   $(card).toggleClass("open");
 }
 
+//adds a card to the array only in case it isnt already there
 function add_displayed_symbol(card) {
-  //adds a card to the array only in case it isnt already there
   if (!displayed_cards.includes(card)) {
     displayed_cards.push(card);
   }
@@ -168,7 +168,7 @@ function reset() {
   $(".fa").toggleClass("fa-star-o", false);
   $(".fa").toggleClass("fa-star", true);
 }
-
+// hides all cards
 function hide() {
   $(".card").each(function() {
     $(this).toggleClass("open", false);
@@ -193,14 +193,3 @@ function timer() {
     document.getElementById("timer").innerHTML = seconds_since_start;
   }, 1000);
 }
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
